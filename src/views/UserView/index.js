6f5c1c1bdaf63288d48c695ff661157496fed7d9
arkine <slyngstad55@gmail.com>
2018-11-04 10:18:52 -0700

    fix transition prop of userview

--- src/views/UserView/index.js
+++ src/views/UserView/index.js
@@ -11,7 +11,7 @@ import {fadeFromLeft} from '../../services/animations/transition'
 
 import {Container} from './styled'
 
-@transitionRoute(fadeFromLeft, {timeout: 5000})
+@transitionRoute(fadeFromLeft)
 export default class UserView extends React.PureComponent {
   static propTypes = {
     user: PropTypes.object.isRequired
